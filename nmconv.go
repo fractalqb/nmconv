515aed913581a3a6c30d7e160bf758907c8f903e
Marcus <marcus@fractalqb.de>
2018-01-09 19:20:14 +0100

    ICI

---
+++ nmconv.go
@@ -0,0 +1,105 @@
+package nmconv
+
+import (
+	"bytes"
+	"strings"
+	"unicode"
+)
+
+type Normalize func(string) []string
+
+type Denormalize func([]string) string
+
+func Convert(str string, from Normalize, to Denormalize) string {
+	tmp := from(str)
+	return to(tmp)
+}
+
+func Sep(separator string) Denormalize {
+	return func(norm []string) string {
+		return strings.Join(norm, separator)
+	}
+}
+
+func SepX(xformWord func(string) string, separator string) Denormalize {
+	return func(norm []string) string {
+		tmp := make([]string, len(norm))
+		for i := 0; i < len(norm); i++ {
+			tmp[i] = xformWord(norm[i])
+		}
+		return strings.Join(tmp, separator)
+	}
+}
+
+func Unsep(separator string) Normalize {
+	return func(str string) []string {
+		return strings.Split(str, separator)
+	}
+}
+
+func SepConvention(separator string) Conversion {
+	return Conversion{
+		Norm:   Unsep(separator),
+		Denorm: Sep(separator),
+	}
+}
+
+func SepXConvention(xformWord func(string) string, separator string) Conversion {
+	return Conversion{
+		Norm:   Unsep(separator),
+		Denorm: SepX(xformWord, separator),
+	}
+}
+
+func capWord(w string) string {
+	if len(w) == 0 {
+		return ""
+	}
+	buf := bytes.NewBuffer(make([]byte, 0, len(w)))
+	buf.WriteString(strings.ToUpper(w[:1]))
+	if w = w[1:]; len(w) > 0 {
+		buf.WriteString(strings.ToLower(w))
+	}
+	return buf.String()
+}
+
+func Camel1Low(norm []string) string {
+	if len(norm) == 0 {
+		return ""
+	} else {
+		buf := bytes.NewBufferString(strings.ToLower(norm[0]))
+		for i := 1; i < len(norm); i++ {
+			buf.WriteString(capWord(norm[i]))
+		}
+		return buf.String()
+	}
+}
+
+func Camel1Up(norm []string) string {
+	buf := bytes.NewBuffer(nil)
+	for _, w := range norm {
+		buf.WriteString(capWord(w))
+	}
+	return buf.String()
+}
+
+func Uncamel(str string) (norm []string) {
+	sep := strings.IndexFunc(str[1:], unicode.IsUpper)
+	for sep >= 0 {
+		sep++
+		norm = append(norm, str[:sep])
+		str = str[sep:]
+		sep = strings.IndexFunc(str[1:], unicode.IsUpper)
+	}
+	norm = append(norm, str)
+	return norm
+}
+
+type Conversion struct {
+	Norm   Normalize
+	Denorm Denormalize
+}
+
+func (cnv Conversion) Convert(str string) string {
+	return Convert(str, cnv.Norm, cnv.Denorm)
+}
---
+++ nmconv_test.go
@@ -0,0 +1,121 @@
+package nmconv
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stvp/assert"
+)
+
+func TestUncamel_1wordLow(t *testing.T) {
+	res := Uncamel("foo")
+	assert.Equal(t, 1, len(res))
+	assert.Equal(t, "foo", res[0])
+}
+
+func TestUncamel_1wordUp(t *testing.T) {
+	res := Uncamel("Foo")
+	assert.Equal(t, 1, len(res))
+	assert.Equal(t, "Foo", res[0])
+}
+
+func TestUncamel_2wordLow(t *testing.T) {
+	res := Uncamel("fooBar")
+	assert.Equal(t, 2, len(res))
+	assert.Equal(t, "foo", res[0])
+	assert.Equal(t, "Bar", res[1])
+}
+
+func TestUncamel_2wordUp(t *testing.T) {
+	res := Uncamel("FooBar")
+	assert.Equal(t, 2, len(res))
+	assert.Equal(t, "Foo", res[0])
+	assert.Equal(t, "Bar", res[1])
+}
+
+func TestUncamel_short(t *testing.T) {
+	res := Uncamel("FB")
+	assert.Equal(t, 2, len(res), res)
+	assert.Equal(t, "F", res[0])
+	assert.Equal(t, "B", res[1])
+}
+
+func TestCamelLow(t *testing.T) {
+	res := Camel1Low([]string{"FOO", "bar"})
+	assert.Equal(t, "fooBar", res)
+}
+
+func TestCamelUp(t *testing.T) {
+	res := Camel1Up([]string{"foo", "BAR"})
+	assert.Equal(t, "FooBar", res)
+}
+
+func TestCamel_shortLow(t *testing.T) {
+	res := Camel1Low([]string{"f", "b"})
+	assert.Equal(t, "fB", res)
+}
+
+func TestCamel_shortUp(t *testing.T) {
+	res := Camel1Up([]string{"f", "b"})
+	assert.Equal(t, "FB", res)
+}
+
+func TestFunConv(t *testing.T) {
+	conv := Conversion{Norm: Unsep("_"), Denorm: Sep("-")}
+	res := conv.Convert("foo_bar_baz")
+	if res != "foo-bar-baz" {
+		t.Error(res)
+	}
+}
+
+func BenchmarkFunConv(b *testing.B) {
+	conv := Conversion{Norm: Unsep("_"), Denorm: Sep("-")}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		conv.Convert("foo_bar_baz")
+	}
+}
+
+func ExamplePassConversion() {
+	user := func(nm string, conv func(string) string) {
+		fmt.Printf("'%s' → '%s'", nm, conv(nm))
+	}
+	conv := Conversion{Norm: Unsep("_"), Denorm: Sep("-")}
+	user("foo_bar_baz", conv.Convert)
+	// Output:
+	// 'foo_bar_baz' → 'foo-bar-baz'
+}
+
+type IfConvn interface {
+	Norm(str string) []string
+	Denorm(n []string) string
+}
+
+type IfSep string
+
+func (s IfSep) Norm(str string) []string {
+	return strings.Split(str, string(s))
+}
+
+func (s IfSep) Denorm(n []string) string {
+	return strings.Join(n, string(s))
+}
+
+type IfCnv struct {
+	From IfConvn
+	To   IfConvn
+}
+
+func (cnv IfCnv) Convert(str string) string {
+	tmp := cnv.From.Norm(str)
+	return cnv.To.Denorm(tmp)
+}
+
+func BenchmarkIfConv(b *testing.B) {
+	conv := IfCnv{From: IfSep("_"), To: IfSep("-")}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		conv.Convert("foo_bar_baz")
+	}
+}
